d7503e57628cac6f04268109145ca61793c21066
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-10-10 13:22:44 -0400

    1 done 3 almost done

---
+++ app.js
@@ -0,0 +1,30 @@
+const songs = [
+    {
+        songname: 'My Iron Lung',
+        artistname: 'Radiohead',
+        Ratings:['Rateyourmuisc.com:4/5', 'Allmuisc.come:8/10', 'IMDB:7.2/10'],
+        awards:'N/A',
+        Year_made: 1995,
+        album:'My Iron Lung'
+    },
+    {
+        songname: 'misery',
+        artistname: 'Jayeds',
+        awards:'N/A',
+        Year_made: 2023,
+        album:'Gheto Cupid'
+    },    
+      {
+        songname: 'Everlong',
+        artistname: 'Foo Fighters',
+        awards:'2009 Nominee VMA. Best Video Foo Fighters',
+        Year_made: 1997,
+        album:'The Colour and the Shape'
+    }
+
+
+]
+console.log(songs)
+age=songs.filter((songs)=>)
+
+/* songs.forEach((songs)=>console.log(songs.Ratings)) */
